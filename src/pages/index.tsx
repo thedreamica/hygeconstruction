25eea9cf717d5ca870373ce57a363b725cc9b616
raghul <raghulkrishna50@gmail.com>
2023-05-12 11:03:33 +0530

    checking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-// import styles from "@/styles/Home.module.css";
 import { Box } from "@mui/material";
 import NavigationBar from "@/components/navBar";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <Box>
-      <NavigationBar /> <Box> Hello Hyge Construction</Box>
+      <NavigationBar /> 
+      <Box> Hello testing</Box>
     </Box>
   );
 }
